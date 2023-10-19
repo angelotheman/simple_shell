24d0214f75ff50249a6dedf37abb8b84ebf90a74
Kwabena Twumasi <ktwum721@gmail.com>
2023-10-19 05:53:51 +0000

    Updates

--- file_loc.c
+++ file_loc.c
@@ -25,27 +25,26 @@ int startsWithForwardSlash(const char *str)
 
 char *get_file_loc(char *path, char *file_name)
 {
-	char *path_copy, *token; /*new_Path = NULL;*/
+	char *path_copy, *token;
 	struct stat file_path;
-	char *path_buffer;
+	char *path_buffer = NULL;
 
-	path_buffer = NULL;
 	path_copy = strdup(path);
 	token = strtok(path_copy, ":");
 
 	while (token)
 	{
 		if (path_buffer)
+		{
+			free(path_buffer);
 			path_buffer = NULL;
-
+		}
 		path_buffer = malloc(strlen(token) + strlen(file_name) + 2);
-
 		if (!path_buffer)
 		{
 			perror("Error: malloc failed");
 			exit(EXIT_FAILURE);
 		}
-
 		strcat(path_buffer, token);
 		strcat(path_buffer, "/");
 		strcat(path_buffer, file_name);
@@ -56,11 +55,11 @@ char *get_file_loc(char *path, char *file_name)
 			free(path_copy);
 			return (path_buffer);
 		}
-
 		token = strtok(NULL, ":");
 	}
 	free(path_copy);
-	free(path_buffer);
+	if (path_buffer)
+		free(path_buffer);
 	return (NULL);
 }
 
@@ -74,6 +73,8 @@ char *get_file_loc(char *path, char *file_name)
 char *get_file_path(char *file_name)
 {
 	char *path = getenv("PATH");
+	char *full_path;
+
 
 	if (startsWithForwardSlash(file_name) &&
 			access(file_name, X_OK) == 0)
@@ -85,5 +86,14 @@ char *get_file_path(char *file_name)
 		return (NULL);
 	}
 
-	return (get_file_loc(path, file_name));
+	full_path = get_file_loc(path, file_name);
+
+	if (full_path == NULL)
+	{
+		write(2, file_name, strlen(file_name));
+		write(2, ": command not found\n", 19);
+		return (NULL);
+	}
+
+	return (full_path);
 }
